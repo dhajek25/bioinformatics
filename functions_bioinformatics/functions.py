e5a8e2910102f16c4b2c913c12319baf02f42a90
David Hajek <st45592@student.upce.cz>
2023-04-16 20:39:06 +0200

    starting project

--- functions_bioinformatics/functions.py
+++ functions_bioinformatics/functions.py
@@ -1,8 +1,32 @@
-# https://rosalind.info/problems/locations/
+s = 'AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC'
 
-# https://github.com/ayigitdogan/Rosalind-Solutions
+def dna_nucleotides_count(s):
+    """
+    Count the nucleotides (A, T, C, G) in the input string.
 
-# https://github.com/topics/rosalind-bioinformatics-challenge?o=desc&s=updated
+    Parameters:
+    s (str): The input string to count nucleotides in.
+
+    Returns:
+    A view object of the dictionary's items. Each item is a tuple with a nucleotide and its count.
+
+    Example:
+    dna_nucleotides_count('AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC')
+    dict_items([('A', 20), ('T', 21), ('C', 12), ('G', 17)])
+    """
+
+    nucl_dict = {'A': 0, 'T': 0, 'C': 0, 'G': 0}
+
+    for e in s:
+        if e in nucl_dict.keys():
+            nucl_dict[e] += 1
+
+    result = ''
+    for key, value in nucl_dict.items():
+        result += f"{key}: {value}\n"
+    return result
+
+result = dna_nucleotides_count(s)
+print(result)
 
 
-import pandas as pd
